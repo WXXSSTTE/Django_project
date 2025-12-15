ef62985dfea3c9601d41b0006661a7e7c950bc8d
WXXSSTTE <vasilisa200492@gmail.com>
2025-12-15 03:53:15 +0300

    added links for bottoms on main page

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('about/', views.about, name = 'about')
+    path('about/', views.about, name = 'about'),
+    path('timer/', views.timer, name='timer'),
+    path('shedule/', views.schedule, name='schedule'),
+    path('progress/', views.progress, name="progress"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
 
 # Create your views here.
 def index(request):
@@ -6,3 +8,12 @@ def index(request):
 
 def about(request):
     return render(request, 'main/about.html')
+
+def timer(request):
+    return HttpResponse("Таймер")
+
+def schedule(request):
+    return HttpResponse("Расписание")
+
+def progress(request):
+    return HttpResponse("Прогресс")
